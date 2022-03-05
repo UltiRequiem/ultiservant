252d3acbd0a2173826731c9dcc19d0203a63c0f2
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-03-05 11:32:00 -0500

    :wrench: Update

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,13 @@
 import "dotenv/config";
 
-export const TOKEN = process.env.DISCORD_TOKEN;
-
 export const ENV = process.env.NODE_ENV;
 
-export const CLIENT_ID = "949492734639620107";
+export const TOKEN =
+  ENV === "development"
+    ? process.env.DISCORD_TOKEN_DEV
+    : process.env.DISCORD_TOKEN;
+
+export const CLIENT_ID =
+  ENV === "development" ? "949702056191619072" : "949492734639620107";
 
 export const GUILD_ID = "940222379865563156";
--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -2,5 +2,6 @@ declare namespace NodeJS {
   interface ProcessEnv {
     readonly NODE_ENV: "development" | "production";
     readonly DISCORD_TOKEN: string;
+    readonly DISCORD_TOKEN_DEV: string;
   }
 }
