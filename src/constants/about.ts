e592d3cac6afe12e2fdceb97e1ce0bb1fb63048e
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-03-06 12:30:18 -0500

    :memo: Update messages

--- src/constants/about.ts
+++ src/constants/about.ts
@@ -4,20 +4,17 @@ Please read the following rules prior to interacting in the server.
     • Keep all messages and content SFW
     • Harassment and Hate Speech is prohibited
     • Do not share pirated/crack software and content
-    • Discussing sensitive topics (civics, religion, health) or is frowned upon
     • No Spamming
     • No Doxxing
     • Send malicious links or viruses is prohibited
     • Asking moderators for leniency in punishment is strictly disallowed
     • Follow Discord’s Terms of Service.
     
-    The rules mentioned here only exist for the safety of members like you. Not being aware of the present list of rules is not a valid excuse to be protected from consequences.
-
     Overall use common sense.
 `;
 
 export const THANK_YOU = `
-  Thank you for joining! If you'd like to support me, you can send out this permanent invite link to anyone that might find the server interesting
+  Thank you for joining! If you'd like to support the community, you can send out this permanent invite link to anyone that might find the server interesting
 
    https://ultirequiem.com/discord
 `;
