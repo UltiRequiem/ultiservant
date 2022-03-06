1f3cc55446874aad6f9757e84fd94f7fc01b3515
Pranshu Patel <70943732+pranshu05@users.noreply.github.com>
2022-03-06 21:18:01 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,38 @@
 import { Client, Intents } from "discord.js";
 import { TOKEN } from "./config";
+import { fs } from "fs";
+const client = new Client({  
+  intents: [
+        'GUILDS',
+        'GUILD_MEMBERS',
+        'GUILD_MESSAGES',
+        'GUILD_MESSAGE_REACTIONS',
+        'GUILD_MESSAGE_TYPING',
+        'GUILD_BANS',
+        'GUILD_EMOJIS_AND_STICKERS',
+        'GUILD_INTEGRATIONS',
+        'GUILD_INVITES',
+        'GUILD_VOICE_STATES',
+        'GUILD_PRESENCES',
+    ] 
+});
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+//command handeler
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
+const commands = []
+client.commands = new Collection()
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`)
+    commands.push(command.data.toJSON())
+	client.commands.set(command.data.name, command)
+}
+
 
 client.once("ready", () => {
   console.log("Ready!");
 
   console.log(client.user?.id);
 });
-
 client.on("interactionCreate", async (interaction) => {
   console.log(interaction);
   if (!interaction.isCommand()) return;
